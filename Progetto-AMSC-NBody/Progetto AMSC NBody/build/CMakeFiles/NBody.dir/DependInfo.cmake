e94127a37dcfabf069154dc8df88a21377137820
Overlord2410 <mtrigam@gmail.com>
2023-01-19 10:30:29 +0000

    added executable

---
+++ Progetto-AMSC-NBody/Progetto AMSC NBody/build/CMakeFiles/NBody.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jellyfish/shared-folder/AMSC/BarnesHut2/ProgettoNBody/Progetto-AMSC-NBody/Progetto AMSC NBody/Profiling.cpp" "CMakeFiles/NBody.dir/Profiling.cpp.o" "gcc" "CMakeFiles/NBody.dir/Profiling.cpp.o.d"
+  "/home/jellyfish/shared-folder/AMSC/BarnesHut2/ProgettoNBody/Progetto-AMSC-NBody/Progetto AMSC NBody/main_bh.cpp" "CMakeFiles/NBody.dir/main_bh.cpp.o" "gcc" "CMakeFiles/NBody.dir/main_bh.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Progetto-AMSC-NBody/Progetto AMSC NBody/build/CMakeFiles/NBody.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NBody.dir/Profiling.cpp.o"
+  "CMakeFiles/NBody.dir/Profiling.cpp.o.d"
+  "CMakeFiles/NBody.dir/main_bh.cpp.o"
+  "CMakeFiles/NBody.dir/main_bh.cpp.o.d"
+  "NBody"
+  "NBody.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NBody.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
